3b57e8db4760d4aff19a70a102a96c21cca60a89
eddiebergman <eddiebergmanhs@gmail.com>
2022-04-19 21:45:44 +0200

    Add pipes test

--- test/test_pipes.py
+++ test/test_pipes.py
@@ -5,6 +5,13 @@ def test_basic() -> None:
     def square(x: int) -> int:
         return x**2
 
-    pipe = daisy.start | square | square
+    def divides(x: int) -> int:
+        return x / 2
+
+    pipe = daisy.start | square | square | divides
+
     assert pipe(3) == 81
     assert pipe(2) == 16
+
+    pipe |= square | square
+
